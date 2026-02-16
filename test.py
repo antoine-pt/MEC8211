86d4e22672d12e2585b00b7b94cce3ef163dc45d
antoine-pt <antoine.payette-toupin@polymtl.ca>
2026-02-15 20:31:40 -0500

    Ajout test invariance galilléenne sur le parametre D

--- test.py
+++ test.py
@@ -18,8 +18,28 @@ class TestSolveur(unittest.TestCase):
 
         # Vérifier que la solution numérique est proche de la solution analytique
         
-        self.assertTrue(sim, "La solution numérique est vide!")
-        self.assertTrue(ana, "La solution analytique est vide!")
+        self.assertTrue(sim.all() != None, "La solution numérique est vide!")
+        self.assertTrue(sim.all() != 0.0, "La solution numérique est vide!")
+        self.assertTrue(ana.all() != None, "La solution analytique est vide!")
+
+    def testInvarianceGallileenne(self):
+        """Test de l'invariance galiléenne du solveur."""
+        nPts = 10
+        R = 1.0
+        D = 1e-10
+        S = 1e-8
+        Ce = 0
+
+        params = Parametres(nPts=nPts, R=R, D=D, S=S, Ce=Ce)
+        sim1 = solveur_centre(params)
+
+        # Modifier les paramètres pour simuler un changement de référentiel
+        params.S *= 2  # Doubler la source
+        sim2 = solveur_centre(params)
+
+        # Vérifier que la solution numérique est proportionnelle à la source
+        # np.allclose vérifie que les deux tableaux sont égaux à une tolérance (prise par défaut) près
+        self.assertTrue(np.allclose(sim2, sim1*2), "Le solveur n'est pas galiléen!")
 
     def testOrdreDeConvergenceAvant(self):
         """Test l'odre de convergence pour le schéma avant."""
