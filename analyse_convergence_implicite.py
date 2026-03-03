078279661b72637ce93f83e4e6af1b6642e0c4ab
antoine-pt <antoine.payette-toupin@polymtl.ca>
2026-03-03 14:27:43 -0500

    feat: plot des courbes L1,L2 et Linf

--- analyse_convergence_implicite.py
+++ analyse_convergence_implicite.py
@@ -6,35 +6,53 @@ import sys
 with open("resultats", mode="r", newline="") as file:   
     argument = sys.argv[1]
     csvr = csv.DictReader(file)
+    L1 = []
     L2 = []
+    Linf = []
     dh = []
     index = 0
     for row in csvr:
-        L2.append(float(row["L2"]))
+
         # temps ou espace
         if argument == "e":
             dh.append(float(row["dr"]))
         elif argument == "t":
             dh.append(float(row["dt"]))
-            print("here")
         else:
             print(argument)
             print("Missing input argument!")
             sys.exit(1)
+
+        L1.append(float(row["L1"]))
+        L2.append(float(row["L2"]))
+        Linf.append(float(row["Linf"]))
+
         index+=1
 
+    L1 = np.asarray(L1)
     L2 = np.asarray(L2)
+    Linf = np.asarray(Linf)
     dh = np.asarray(dh)
     
-    ordres = np.log(L2[:-1]/L2[1:])/np.log(dh[:-1]/dh[1:])
-    print(ordres)
+    ordresL1 = np.log(L1[:-1]/L1[1:])/np.log(dh[:-1]/dh[1:])
+    ordresL2 = np.log(L2[:-1]/L2[1:])/np.log(dh[:-1]/dh[1:])
+    ordresLinf = np.log(Linf[:-1]/Linf[1:])/np.log(dh[:-1]/dh[1:])
+
+    print(ordresL1)
+
+    print(ordresL2)
+
+    print(ordresLinf)
+
     string = ""
-    for i in range(len(ordres)):
-        string += f"Ordre de convergence entre les résolutions {i} et {i+1} : {ordres[i]:.7f} \n"
+    for i in range(len(ordresL2)):
+        string += f"Ordre de convergence L2 entre les résolutions {i} et {i+1} : {ordresL2[i]:.7f} \n"
     text = (string)
 
     plt.figure()
+    plt.loglog(dh,L1,"x-", label="L1")
     plt.loglog(dh,L2,"x-", label="L2")
+    plt.loglog(dh,Linf,"x-", label="Linf")
     plt.legend()
     plt.xlabel("pas de discrétisation")
     plt.ylabel("erreur")
